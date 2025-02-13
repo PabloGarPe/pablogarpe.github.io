7c51e119cfdbfc165367b04a06fd57f6d9cb2c70
Pablo <pablogarciapernas@gmail.com>
2025-02-13 12:46:02 +0100

    Fixed the img

--- components/projects.tsx
+++ components/projects.tsx
@@ -6,11 +6,11 @@ export const Projects = () => {
       <section className="relative scroll-mt-[4.5rem]" id="projects">
         <div className="absolute inset-0 pointer-events-none -z-[1]"></div>
         <div className="rmx-auto w-full relative md:px-6 text-default lg:py-20 md:py-10 px-4 py-6">
-          <div className="ml-auto max-w-7xl p-4">
-            <div className="md:flex md:gap-10 md:justify-center">
-              <div className="self-center w-full">
-                <div className="text-lg mb-12 text-center">
-                  <h3 className="font-bold mb-2 tracking-tight text-4xl">
+          <div className="ml-auto p-4">
+            <div className="md:flex md:flex-col md:gapy-10 md:justify-center">
+              <div className="md:self-center w-full md:flex md:flex-col md:justify-center">
+                <div className="mb-16 w-full md:flex md:flex-col justify-center">
+                  <h3 className="font-bold tracking-tight text-4xl text-center">
                     My Projects
                   </h3>
                 </div>
@@ -20,7 +20,7 @@ export const Projects = () => {
                       <a href="#">MathTexpedia</a>
                     </h3>
                   </div>
-                  <div className="md:justify-end pl-12">
+                  <div className="md:justify-end pl-12 lg:pl-48 xl:pl-60">
                     <Image src="/mathtexpedia.png" alt="MathTexpedia project" width={500} height={300} />
                   </div>
                 </div>
