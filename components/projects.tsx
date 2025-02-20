c412e4fca34eb974a763321e509b668f358d9379
Pablo <pablogarciapernas@gmail.com>
2025-02-20 15:40:55 +0100

    Project added

--- components/projects.tsx
+++ components/projects.tsx
@@ -39,13 +39,48 @@ export const Projects = () => {
                       </p>
                     </div>
                   </div>
-                  <div className="flex flex-col items-center align-middle justify-center md:ml-10">
-                    <Image
+                  <div className="flex flex-col items-center align-middle justify-center">
+                    <img
                       src="/mathtexpedia.png"
                       alt="MathTexpedia project"
                       width={400}
                       height={250}
                     />
+                  </div>
+                </div>
+                <div className="flex flex-col md:grid md:grid-cols-2 mt-20 ">
+                  <div className="flex flex-col items-center align-middle justify-center">
+                    <img
+                      src="/horariospceo.png"
+                      alt="MathTexpedia project"
+                      width={400}
+                      height={250}
+                    />
+                  </div>
+                  <div className="flex flex-col items-center align-middle justify-center ">
+                    <div className="text-lg mb-12 lg:max-w-[75%]">
+                      <div className="flex flex-col items-center align-middle justify-center">
+                        <h3 className="font-bold mb-2 tracking-tight text-2xl">
+                          <a
+                            href="https://horariospceo.web.app/"
+                            target="blank"
+                            className="hover:text-pink-500 hover:transition hover:ease-linear   hover:duration-75"
+                          >
+                            HorariosPCEO
+                          </a>
+                        </h3>
+                      </div>
+                      <p className="text-justify text-wrap mb-2">
+                        HorariosPCEO is a useful web application that allows me
+                        and my mates on my degree to check both schedules (with
+                        exams and classes) and make it update all the weeks.
+                      </p>
+                      <p className="text-justify text-wrap">
+                        It is built with Next.js, Tailwind CSS and FastAPI. The
+                        application is deployed in Render and hosted in
+                        Firebase.
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
