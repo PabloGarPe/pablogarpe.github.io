fe84905a084c6e799fedc2ba21ed460c05f36a35
Pablo Garcia <pablogarciapernas@gmail.com>
2025-03-22 10:28:40 +0100

    Fixed styles, added mini dropdown menu

--- components/header.tsx
+++ components/header.tsx
@@ -19,20 +19,23 @@ export const Header = () => {
     };
   }, []);
 
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <header
       className={`mx-auto w-full transition-opacity duration-[0.15s] ease-in-out flex-none sticky top-0 z-50 ${
-        scrolled
-          ? "backdrop-blur-[6px] shadow-sm shadow-gray-900 border-b border-gray-400"
+        scrolled && !menuOpen
+          ? "backdrop-blur-[6px] shadow-md shadow-gray-900 border-b border-gray-400"
           : ""
-      }`}
+      }
+      `}
     >
       <div
         className={`absolute z-0 transition-opacity duration-300 ease-in-out w-full h-full top-0 left-0
-           ${scrolled ? "scrolled" : ""}`}
+           ${scrolled && !menuOpen ? "scrolled" : ""}`}
       ></div>
       <div className="relative md:px-3 text-default md:grid md:grid-cols-[0.2fr_2fr_auto] px-3 py-3 w-full">
-        <div className="flex justify-between md:jsutify-self-start">
+        <div className="hidden md:flex justify-between md:jsutify-self-start">
           <a
             className="text-decoration-none flex items-center mr-auto"
             href="/"
@@ -78,13 +81,83 @@ export const Header = () => {
             </li>
           </ul>
         </nav>
+        <nav className="md:hidden grid grid-cols-2 space-x-10 justify-center text-default relative w-full">
+          <a
+            className="text-decoration-none flex items-center mr-auto"
+            href="/"
+          >
+            <span className="ml-10 font-bold md:text-xl text-2xl whitespace-nowrap">
+              Pablo García
+            </span>
+          </a>
+          <div className="flex justify-end">
+            <button
+              className="bg-pink-500 hover:bg-pink-300 font-bold px-4 py-2 rounded-full text-white transition-colors w-20"
+              onClick={() => setMenuOpen(!menuOpen)}
+            >
+              Menu
+            </button>
+          </div>
+          {menuOpen && (
+            <div className="dropdown fixed right-0 w-52 mt-16 bg-black text-white overflow-hidden border border-gray-400 rounded-md shadow-md">
+              <ul className="flex flex-col justify-end items-end space-y-10 tracking-[0.05em] font-bold flex-nowrap overflow-hidden p-4 py-10">
+                <li>
+                  <a
+                    className="px-4 py-3 hover:bg-gray-200 hover:text-black hover:rounded-full hover:shadow-inner overflow-hidden"
+                    href="/#home"
+                    onClick={() => setMenuOpen(false)}
+                  >
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="px-4 py-3 hover:bg-gray-200 hover:text-black hover:rounded-full hover:shadow-inner overflow-hidden"
+                    href="/#info"
+                    onClick={() => setMenuOpen(false)}
+                  >
+                    About Me
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="px-4 py-3 hover:bg-gray-200 hover:text-black hover:rounded-full hover:shadow-inner overflow-hidden"
+                    href="/#projects"
+                    onClick={() => setMenuOpen(false)}
+                  >
+                    Projects
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="px-4 py-3 hover:bg-gray-200 hover:text-black hover:rounded-full hover:shadow-inner overflow-hidden"
+                    href="mailto:pablogarciapernas@gmail.com"
+                    onClick={() => setMenuOpen(false)}
+                  >
+                    Contact
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/Curriculum.pdf"
+                    download="Pablo_Garcia_Curriculum.pdf"
+                    className="bg-pink-500 hover:bg-pink-300 font-bold px-4 py-2 rounded-full text-white transition-colors"
+                  >
+                    Resume
+                  </a>
+                </li>
+              </ul>
+            </div>
+          )}
+        </nav>
         <div className="md:flex md:flex-col hidden md:items-end md:justify-self-end">
-            <a href="/Curriculum.pdf"
+          <a
+            href="/Curriculum.pdf"
             download="Pablo_Garcia_Curriculum.pdf"
             className="bg-pink-500 hover:bg-pink-300 font-bold px-4 py-2 rounded-full text-white transition-colors"
-            >
+          >
             Resume
-            </a>
+          </a>
         </div>
       </div>
     </header>
--- components/projects.tsx
+++ components/projects.tsx
@@ -22,7 +22,7 @@ export const Projects = () => {
                           <a
                             href="https://mathtexpedia.web.app/"
                             target="blank"
-                            className="hover:text-pink-500 hover:transition hover:ease-linear   hover:duration-75"
+                            className="hover:text-pink-500 hover:transition hover:ease-linear hover:duration-75 cursor-pointer"
                           >
                             MathTexpedia
                           </a>
@@ -40,22 +40,26 @@ export const Projects = () => {
                     </div>
                   </div>
                   <div className="flex flex-col items-center align-middle justify-center">
+                    <a href="https://mathtexpedia.web.app/" target="blank">
                     <img
                       src="/mathtexpedia.png"
                       alt="MathTexpedia project"
                       width={400}
                       height={250}
                     />
+                    </a>
                   </div>
                 </div>
-                <div className="flex flex-col md:grid md:grid-cols-2 mt-20 ">
+                <div className="hidden md:grid md:grid-cols-2 mt-20 ">
                   <div className="flex flex-col items-center align-middle justify-center">
+                    <a href="https://mathtexpedia.web.app/" target="blank">
                     <img
                       src="/horariospceo.png"
                       alt="MathTexpedia project"
                       width={400}
                       height={250}
                     />
+                    </a>
                   </div>
                   <div className="flex flex-col items-center align-middle justify-center ">
                     <div className="text-lg mb-12 lg:max-w-[75%]">
@@ -64,7 +68,7 @@ export const Projects = () => {
                           <a
                             href="https://horariospceo.web.app/"
                             target="blank"
-                            className="hover:text-pink-500 hover:transition hover:ease-linear   hover:duration-75"
+                            className="hover:text-pink-500 hover:transition hover:ease-linear cursor-pointer hover:duration-75"
                           >
                             HorariosPCEO
                           </a>
@@ -81,6 +85,43 @@ export const Projects = () => {
                         Firebase.
                       </p>
                     </div>
+                  </div>
+                </div>
+                <div className="md:hidden flex flex-col mt-20 ">
+                  <div className="flex flex-col items-center align-middle justify-center ">
+                    <div className="text-lg mb-12 lg:max-w-[75%]">
+                      <div className="flex flex-col items-center align-middle justify-center">
+                        <h3 className="font-bold mb-2 tracking-tight text-2xl">
+                          <a
+                            href="https://horariospceo.web.app/"
+                            target="blank"
+                            className="hover:text-pink-500 hover:transition hover:ease-linear cursor-pointer hover:duration-75"
+                          >
+                            HorariosPCEO
+                          </a>
+                        </h3>
+                      </div>
+                      <p className="text-justify text-wrap mb-2">
+                        HorariosPCEO is a useful web application that allows me
+                        and my mates on my degree to check both schedules (with
+                        exams and classes) and make it update all the weeks.
+                      </p>
+                      <p className="text-justify text-wrap">
+                        It is built with Next.js, Tailwind CSS and FastAPI. The
+                        application is deployed in Render and hosted in
+                        Firebase.
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex flex-col items-center align-middle justify-center">
+                    <a href="https://horariospceo.web.app/" target="blank">
+                    <img
+                      src="/horariospceo.png"
+                      alt="MathTexpedia project"
+                      width={400}
+                      height={250}
+                    />
+                    </a>
                   </div>
                 </div>
               </div>
